ad5bf7bcabd999abcf64e1704dd3af6f893cd74f
kGe-z <1758789859@qq.com>
2024-08-12 17:30:31 +0800

    feat: add wasm demo

---
+++ demo/file_block.test.js
@@ -0,0 +1,13 @@
+const { resolve } = require("path");
+
+const generateFileBlockDigest = require("..");
+
+const run = async () => {
+  const fileBlockDigest = await generateFileBlockDigest(
+    resolve(__dirname, "./sha_calc_demo.txt")
+  );
+
+  console.log(fileBlockDigest);
+};
+
+run();
